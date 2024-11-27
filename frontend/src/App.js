e389a5c71ce66d59eca059698ce1d49541488458
IK <khushijukanti33@gmail.com>
2024-11-27 19:07:26 +0530

    change the path directory importing dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className='Bg-img'>
       <div>
-        <Dashboard familyId={familyId} />
+        <Dashboard familyId={familyId}/>
         <AddTransactionForm />
       </div>
 
