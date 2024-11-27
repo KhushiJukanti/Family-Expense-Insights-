1c02d6f4ffa759ccad6b4269e4323a0fcad62c75
IK <khushijukanti33@gmail.com>
2024-11-27 16:10:23 +0530

    change the require path for transactioncontrollers

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { addTransaction, getTransactionsByFamilyId } = require('../controllers/transactionController');
+const { addTransaction, getTransactionsByFamilyId } = require('../controllers/transactioncontrollers');
 
 router.post('/add-transaction', addTransaction);
 
