c7a7ca925c049f2af9a4e6e3b2755f8a965b5081
IK <khushijukanti33@gmail.com>
2024-11-27 18:24:01 +0530

    import the dashbord and add transactions components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+// src/App.js
+
+import React from 'react';
+import './App.css'
+import Dashboard from './Components/Dashboard';
+import AddTransactionForm from './Components/AddTransaction';
 
 function App() {
+  const familyId = 'FAM001'; // This can be dynamically set
+
   return (
-    <div className="App">
-      Hi
+    <div className='Bg-img'>
+      <div>
+        <Dashboard familyId={familyId} />
+        <AddTransactionForm />
+      </div>
+
     </div>
   );
 }
